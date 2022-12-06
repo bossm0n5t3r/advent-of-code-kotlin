94079fd7cac05ade8439a22995b4e8eef00dc44f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-12-07 02:20:05 +0900

    --- Day 6: Tuning Trouble ---

---
+++ src/aoc2022/Day6.kt
@@ -0,0 +1,24 @@
+package aoc2022
+
+import utils.asInputStream
+import utils.verify
+
+/**
+ * --- Day 6: Tuning Trouble ---
+ */
+fun main() {
+    fun startOfMessageMarker(marker: Int): Int {
+        val dataStreamBuffer = "2022/Day6.txt".asInputStream().bufferedReader().readText()
+        (0 until dataStreamBuffer.length - marker).forEach {
+            val tmp = dataStreamBuffer.slice(it until it + marker)
+            if (tmp.toSet().size == marker) return it + marker
+        }
+        return -1
+    }
+
+    fun part1() = startOfMessageMarker(4)
+    fun part2() = startOfMessageMarker(14)
+
+    (part1() to 1210).verify()
+    (part2() to 3476).verify()
+}
