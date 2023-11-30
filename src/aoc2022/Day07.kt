1888c921811bb190d0476af24dfe117071c7e2e8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-01 03:50:10 +0900

    --- Day 7: No Space Left On Device ---

--- src/aoc2022/Day07.kt
+++ src/aoc2022/Day07.kt
@@ -25,21 +25,23 @@ fun main() {
                     if (targetDir == "..") {
                         curDir = curDir.split("/").dropLast(1).joinToString("/")
                     } else if (targetDir != "/") {
-                        curDir = if (curDir != "/") {
-                            "$curDir/$targetDir"
-                        } else {
-                            "$curDir$targetDir"
-                        }
+                        curDir =
+                            if (curDir != "/") {
+                                "$curDir/$targetDir"
+                            } else {
+                                "$curDir$targetDir"
+                            }
                         dirs[curDir] = mutableSetOf()
                     }
                 }
             } else if (splitLine.first() == "dir") {
                 val targetDir = splitLine[1]
-                val newDir = if (curDir != "/") {
-                    "$curDir/$targetDir"
-                } else {
-                    "$curDir$targetDir"
-                }
+                val newDir =
+                    if (curDir != "/") {
+                        "$curDir/$targetDir"
+                    } else {
+                        "$curDir$targetDir"
+                    }
                 dirs[newDir] = mutableSetOf()
             } else {
                 val newFile = File(splitLine.last(), splitLine[0].toLong())
@@ -70,9 +72,10 @@ fun main() {
 
     fun part2(): Long {
         val dirs = getDirsWithFiles()
-        val dirToTotalSize = dirs.mapValues { (_, value) ->
-            value.sumOf { it.size }
-        }
+        val dirToTotalSize =
+            dirs.mapValues { (_, value) ->
+                value.sumOf { it.size }
+            }
         val totalAmountOfUsedSpace = dirToTotalSize["/"] ?: error("NOT FOUND /")
         val unusedSpace = 70000000L - totalAmountOfUsedSpace
         val needMoreSpace = 30000000L - unusedSpace
