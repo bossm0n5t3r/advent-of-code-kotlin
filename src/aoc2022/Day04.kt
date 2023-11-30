24d6d469b52a10733b9175c9cd46fa97e4807966
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-01 03:49:10 +0900

    --- Day 4: Camp Cleanup ---

--- src/aoc2022/Day04.kt
+++ src/aoc2022/Day04.kt
@@ -10,10 +10,11 @@ fun main() {
     fun part1(): Int {
         var result = 0
         "2022/Day04.txt".readInputForEachLine { line ->
-            val (firstSections, secondSections) = line.split(",").map { sectionPair ->
-                val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
-                (startSection..endSection).toSet()
-            }
+            val (firstSections, secondSections) =
+                line.split(",").map { sectionPair ->
+                    val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
+                    (startSection..endSection).toSet()
+                }
             if (firstSections.containsAll(secondSections) || secondSections.containsAll(firstSections)) {
                 result++
             }
@@ -24,10 +25,11 @@ fun main() {
     fun part2(): Int {
         var result = 0
         "2022/Day04.txt".readInputForEachLine { line ->
-            val (firstSections, secondSections) = line.split(",").map { sectionPair ->
-                val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
-                (startSection..endSection).toSet()
-            }
+            val (firstSections, secondSections) =
+                line.split(",").map { sectionPair ->
+                    val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
+                    (startSection..endSection).toSet()
+                }
             if (firstSections.intersect(secondSections).isNotEmpty()) {
                 result++
             }
