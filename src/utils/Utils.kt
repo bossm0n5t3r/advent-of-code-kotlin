e069eae469dce8b7535054b39a336eeeac2827ac
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-01 03:53:40 +0900

    fix: Edit Utils.kt

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -9,16 +9,18 @@ fun String.asInputStream() = File("src/resources/aoc$this").inputStream()
 /**
  * Reads and works for each line from the given input txt file.
  */
-fun String.readInputForEachLine(work: (String) -> Unit) = this.asInputStream().bufferedReader().forEachLine {
-    work(it)
-}
+fun String.readInputForEachLine(work: (String) -> Unit) =
+    this.asInputStream().bufferedReader().forEachLine {
+        work(it)
+    }
 
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun String.md5() =
+    BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * Print first and Verify first == second
