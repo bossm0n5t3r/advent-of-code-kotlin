08962d78eb50f878da08b56a07a564bc4ec0afd6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-02 10:00:51 +0900

    --- Day 1: Trebuchet?! ---

---
+++ src/aoc2023/Day01.kt
@@ -0,0 +1,50 @@
+package aoc2023
+
+import utils.readInput
+import utils.verify
+
+/**
+ * --- Day 1: Trebuchet?! ---
+ */
+fun main() {
+    val inputFile = "2023/Day01.txt"
+
+    fun part1(): Int {
+        return inputFile.readInput().sumOf { line ->
+            line.filter { it.isDigit() }
+                .let { "${it.first()}${it.last()}".toInt() }
+        }
+    }
+
+    fun part2(): Int {
+        val numbers =
+            listOf(
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            )
+        return inputFile
+            .readInput().sumOf { line ->
+                val indexToInt =
+                    numbers.mapIndexed { index, number ->
+                        line.indexOf(number) to index
+                    } +
+                        numbers.mapIndexed { index, number ->
+                            line.lastIndexOf(number) to index
+                        } +
+                        line.mapIndexed { index, c ->
+                            if (c.isDigit()) {
+                                index to c.digitToInt()
+                            } else {
+                                -1 to -1
+                            }
+                        }
+
+                indexToInt
+                    .filter { it.first != -1 }
+                    .sortedBy { it.first }
+                    .let { it.first().second * 10 + it.last().second }
+            }
+    }
+
+    (part1() to 55477).verify()
+    (part2() to 54431).verify()
+}
