d88ab69eeb77652382766f5c0cbd7fded5139f36
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-04 03:50:00 +0900

    --- Day 2: Cube Conundrum ---

--- src/aoc2023/Day02.kt
+++ src/aoc2023/Day02.kt
@@ -9,59 +9,19 @@ import utils.verify
 fun main() {
     val inputFile = "2023/Day02.txt"
 
-    data class Cubes(
-        val red: Int = 0,
-        val green: Int = 0,
-        val blue: Int = 0,
-    ) {
-        fun isPossible(
-            redCubes: Int,
-            greenCubes: Int,
-            blueCubes: Int,
-        ): Boolean {
-            return red <= redCubes && green <= greenCubes && blue <= blueCubes
-        }
-
-        fun update(otherCubes: Cubes): Cubes {
-            return Cubes(
-                red = maxOf(this.red, otherCubes.red),
-                green = maxOf(this.green, otherCubes.green),
-                blue = maxOf(this.blue, otherCubes.blue),
-            )
-        }
-
-        fun power() = red * green * blue
-    }
-
-    fun List<String>.parseCube(cubeColor: String): Int {
-        return this.firstOrNull { it.contains(cubeColor) }
-            ?.replace(cubeColor, "")
-            ?.trim()
-            ?.toInt()
-            ?: 0
-    }
-
-    fun String.toCubes(): Cubes {
-        val splitSubsetOfCubes = this.split(",")
-        return Cubes(
-            red = splitSubsetOfCubes.parseCube("red"),
-            green = splitSubsetOfCubes.parseCube("green"),
-            blue = splitSubsetOfCubes.parseCube("blue"),
-        )
-    }
-
     fun part1(
         redCubes: Int,
         greenCubes: Int,
         blueCubes: Int,
     ): Int {
         return inputFile.readInput().sumOf { line ->
-            val id = line.substringBefore(":").replace("Game", "").trim().toInt()
-            val subSets = line.substringAfter("Game $id: ")
-            for (subSetOfCubes in subSets.split(";")) {
-                val cubes = subSetOfCubes.toCubes()
-                if (cubes.isPossible(redCubes, greenCubes, blueCubes).not()) {
-                    return@sumOf 0
+            val splitLine = line.split(':', ';', ',').map { it.trim().split(" ") }
+            val id = splitLine.first().last().toInt()
+            splitLine.drop(1).forEach { (count, color) ->
+                when (color) {
+                    "red" -> if (count.toInt() > redCubes) return@sumOf 0
+                    "green" -> if (count.toInt() > greenCubes) return@sumOf 0
+                    "blue" -> if (count.toInt() > blueCubes) return@sumOf 0
                 }
             }
             id
@@ -70,14 +30,19 @@ fun main() {
 
     fun part2(): Int {
         return inputFile.readInput().sumOf { line ->
-            val id = line.substringBefore(":").replace("Game", "").trim().toInt()
-            val subSets = line.substringAfter("Game $id: ")
-            var fewestCubes = Cubes()
-            for (subSetOfCubes in subSets.split(";")) {
-                val cubes = subSetOfCubes.toCubes()
-                fewestCubes = fewestCubes.update(cubes)
-            }
-            fewestCubes.power()
+            line
+                .split(':', ';', ',')
+                .map { it.trim().split(" ") }
+                .drop(1)
+                .groupBy { it.last() }
+                .map { (_, values) ->
+                    values
+                        .maxOfOrNull { it.first().toInt() }
+                        ?: 1
+                }
+                .let {
+                    it.reduce { acc, i -> acc * i }
+                }
         }
     }
 
