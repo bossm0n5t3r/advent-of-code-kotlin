4ea0cf515e05e80b57fa152d30a99fa5ee31c88f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-07 05:31:10 +0900

    --- Day 5: If You Give A Seed A Fertilizer ---

--- src/aoc2023/Day05.kt
+++ src/aoc2023/Day05.kt
@@ -8,140 +8,85 @@ import utils.verify
  */
 fun main() {
     val inputFile = "2023/Day05.txt"
+    val whitespace = " "
+    val newLine = "\n"
 
     data class Mapping(val destinationRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long) {
-        fun toDestinationOrNull(source: Long): Long? {
-            if (source >= sourceRangeStart && source < sourceRangeStart + rangeLength) {
-                return source - sourceRangeStart + destinationRangeStart
-            }
-            return null
-        }
+        fun contains(target: Long) = sourceRangeStart <= target && target < sourceRangeStart + rangeLength
 
-        fun toSourceOrNull(destination: Long): Long? {
-            if (destination >= destinationRangeStart && destination < destinationRangeStart + rangeLength) {
-                return destination - destinationRangeStart + sourceRangeStart
-            }
-            return null
+        fun toDestination(source: Long): Long {
+            return source - sourceRangeStart + destinationRangeStart
         }
     }
 
-    fun String.toMapping(): Mapping {
-        val split = this.split(" ")
-        return Mapping(
-            split[0].toLong(),
-            split[1].toLong(),
-            split[2].toLong(),
-        )
-    }
-
-    data class AllMappings(
-        val seedToSoils: MutableList<Mapping> = mutableListOf(),
-        val soilToFertilizers: MutableList<Mapping> = mutableListOf(),
-        val fertilizerToWaters: MutableList<Mapping> = mutableListOf(),
-        val waterToLights: MutableList<Mapping> = mutableListOf(),
-        val lightToTemperatures: MutableList<Mapping> = mutableListOf(),
-        val temperatureToHumidities: MutableList<Mapping> = mutableListOf(),
-        val humidityToLocations: MutableList<Mapping> = mutableListOf(),
-    )
-
-    fun parseAllMappings(lines: List<String>): AllMappings {
-        val allMappings = AllMappings()
-        var index = 0
-        while (index < lines.size) {
-            val line = lines[index]
-            val target =
-                when (line) {
-                    "seed-to-soil map:" -> allMappings.seedToSoils
-                    "soil-to-fertilizer map:" -> allMappings.soilToFertilizers
-                    "fertilizer-to-water map:" -> allMappings.fertilizerToWaters
-                    "water-to-light map:" -> allMappings.waterToLights
-                    "light-to-temperature map:" -> allMappings.lightToTemperatures
-                    "temperature-to-humidity map:" -> allMappings.temperatureToHumidities
-                    "humidity-to-location map:" -> allMappings.humidityToLocations
-                    else -> {
-                        index++
-                        continue
+    fun List<String>.toMappingsList() =
+        this
+            .drop(2)
+            .joinToString(newLine)
+            .split("$newLine$newLine")
+            .map { section ->
+                section.lines().drop(1).map { line ->
+                    line.split(whitespace).map { it.toLong() }.let { (dest, src, length) ->
+                        Mapping(dest, src, length)
                     }
                 }
-            while (index + 1 < lines.size && lines[index + 1].isNotBlank()) {
-                target.add(lines[index + 1].toMapping())
-                index++
             }
-            index++
-        }
-        return allMappings
-    }
 
     fun part1() =
         solve(inputFile) { lines ->
             val seeds =
                 lines
                     .first()
-                    .substringAfter("seeds: ")
-                    .split(" ")
+                    .substringAfter(whitespace)
+                    .split(whitespace)
                     .map { it.toLong() }
 
-            val allMappings = parseAllMappings(lines)
-
-            fun List<Mapping>.getOrDefaultToDestination(source: Long): Long {
-                return this.firstNotNullOfOrNull { it.toDestinationOrNull(source) } ?: source
-            }
+            val mappingsList = lines.toMappingsList()
 
             seeds.minOf { seed ->
-                allMappings.seedToSoils.getOrDefaultToDestination(seed)
-                    .let { allMappings.soilToFertilizers.getOrDefaultToDestination(it) }
-                    .let { allMappings.fertilizerToWaters.getOrDefaultToDestination(it) }
-                    .let { allMappings.waterToLights.getOrDefaultToDestination(it) }
-                    .let { allMappings.lightToTemperatures.getOrDefaultToDestination(it) }
-                    .let { allMappings.temperatureToHumidities.getOrDefaultToDestination(it) }
-                    .let { allMappings.humidityToLocations.getOrDefaultToDestination(it) }
+                mappingsList.fold(seed) { acc, mappings ->
+                    mappings.firstOrNull { it.contains(acc) }
+                        ?.toDestination(acc)
+                        ?: acc
+                }
             }
         }
 
     fun part2() =
         solve(inputFile) { lines ->
             data class Seed(val start: Long, val range: Long) {
-                fun contains(target: Long) = target >= start && target < start + range
+                val seedRange = start..<(start + range)
             }
 
             val seeds =
                 lines
                     .first()
-                    .substringAfter("seeds: ")
-                    .split(" ")
+                    .substringAfter(whitespace)
+                    .split(whitespace)
                     .map { it.toLong() }
                     .chunked(2)
-                    .map {
-                        val (start, range) = it
-                        Seed(start, range)
+                    .map { Seed(it.first(), it.last()) }
+
+            val mappingsList = lines.toMappingsList()
+
+            seeds
+                .flatMap { seed ->
+                    mappingsList.fold(listOf(seed.seedRange)) { acc, mappings ->
+                        acc.flatMap {
+                            mappings.mapNotNull { (dest, src, range) ->
+                                (maxOf(src, it.first) to minOf(src + range, it.last))
+                                    .let { (start, end) ->
+                                        if (start <= end) {
+                                            (dest - src).let { (start + it)..(end + it) }
+                                        } else {
+                                            null
+                                        }
+                                    }
+                            }
+                        }
                     }
-
-            val allMappings = parseAllMappings(lines)
-
-            fun List<Mapping>.getOrDefaultToSource(source: Long): Long {
-                return this.firstNotNullOfOrNull { it.toSourceOrNull(source) } ?: source
-            }
-
-            fun List<Seed>.contains(target: Long): Boolean {
-                return this.any { it.contains(target) }
-            }
-
-            var location = 0L
-            while (location < Long.MAX_VALUE) {
-                val existSeed =
-                    allMappings.humidityToLocations.getOrDefaultToSource(location)
-                        .let { allMappings.temperatureToHumidities.getOrDefaultToSource(it) }
-                        .let { allMappings.lightToTemperatures.getOrDefaultToSource(it) }
-                        .let { allMappings.waterToLights.getOrDefaultToSource(it) }
-                        .let { allMappings.fertilizerToWaters.getOrDefaultToSource(it) }
-                        .let { allMappings.soilToFertilizers.getOrDefaultToSource(it) }
-                        .let { allMappings.seedToSoils.getOrDefaultToSource(it) }
-                        .let { seeds.contains(it) }
-                if (existSeed) {
-                    return@solve location
                 }
-                location++
-            }
+                .minOf { it.first }
         }
 
     (part1() to 51752125L).verify()
