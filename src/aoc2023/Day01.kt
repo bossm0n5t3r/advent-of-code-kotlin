3ab572fb2aa98528c27613ce4e2d77954be9a499
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-13 17:17:20 +0900

    --- Day 1: Trebuchet?! ---

--- src/aoc2023/Day01.kt
+++ src/aoc2023/Day01.kt
@@ -1,16 +1,14 @@
 package aoc2023
 
-import utils.readInput
+import utils.getInputFile
 import utils.verify
 
 /**
  * --- Day 1: Trebuchet?! ---
  */
 fun main() {
-    val inputFile = "2023/Day01.txt"
-
     fun part1(): Int {
-        return inputFile.readInput().sumOf { line ->
+        return getInputFile().readLines().sumOf { line ->
             line.filter { it.isDigit() }
                 .let { "${it.first()}${it.last()}".toInt() }
         }
@@ -21,28 +19,27 @@ fun main() {
             listOf(
                 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
             )
-        return inputFile
-            .readInput().sumOf { line ->
-                val indexToInt =
+        return getInputFile().readLines().sumOf { line ->
+            val indexToInt =
+                numbers.mapIndexed { index, number ->
+                    line.indexOf(number) to index
+                } +
                     numbers.mapIndexed { index, number ->
-                        line.indexOf(number) to index
+                        line.lastIndexOf(number) to index
                     } +
-                        numbers.mapIndexed { index, number ->
-                            line.lastIndexOf(number) to index
-                        } +
-                        line.mapIndexed { index, c ->
-                            if (c.isDigit()) {
-                                index to c.digitToInt()
-                            } else {
-                                -1 to -1
-                            }
+                    line.mapIndexed { index, c ->
+                        if (c.isDigit()) {
+                            index to c.digitToInt()
+                        } else {
+                            -1 to -1
                         }
+                    }
 
-                indexToInt
-                    .filter { it.first != -1 }
-                    .sortedBy { it.first }
-                    .let { it.first().second * 10 + it.last().second }
-            }
+            indexToInt
+                .filter { it.first != -1 }
+                .sortedBy { it.first }
+                .let { it.first().second * 10 + it.last().second }
+        }
     }
 
     (part1() to 55477).verify()
