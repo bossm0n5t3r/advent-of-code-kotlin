f1a77815b1c70842e6cac58f3525aef1c2c796ff
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-18 01:49:10 +0900

    fix: Edit Geometry.kt
    - Add Point

--- src/utils/Geometry.kt
+++ src/utils/Geometry.kt
@@ -1,5 +1,7 @@
 package utils
 
+import kotlin.math.abs
+
 enum class Direction {
     NORTH,
     EAST,
@@ -19,4 +21,13 @@ enum class Direction {
 enum class Turn {
     RIGHT,
     LEFT,
+}
+
+data class Point(
+    val x: Int,
+    val y: Int,
+) {
+    constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
+
+    fun manhattan(other: Point) = abs(x - other.x) + abs(y - other.y)
 }
