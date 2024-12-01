75f4344134b4d9acc128e9fb5713bd1aff827865
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-02 03:39:00 +0900

    --- Day 1: Calorie Counting ---

--- src/main/kotlin/aoc2022/Day01.kt
+++ src/main/kotlin/aoc2022/Day01.kt
@@ -1,72 +1,73 @@
 package aoc2022
 
-import utils.readInputForEachLine
-import utils.verify
+import utils.solve
 
 /**
  * --- Day 1: Calorie Counting ---
  */
-fun main() {
-    fun part1(): Long {
-        var maxTotalCalories = 0L
-        var totalCalories = 0L
-        "2022/Day01.txt".readInputForEachLine {
-            if (it.isBlank()) {
-                if (totalCalories > maxTotalCalories) {
-                    maxTotalCalories = totalCalories
+object Day01 {
+    fun part1(): Long =
+        solve(2022) { lines ->
+            var maxTotalCalories = 0L
+            var totalCalories = 0L
+            lines.forEach {
+                if (it.isBlank()) {
+                    if (totalCalories > maxTotalCalories) {
+                        maxTotalCalories = totalCalories
+                    }
+                    totalCalories = 0L
+                } else {
+                    totalCalories += it.toLong()
                 }
-                totalCalories = 0L
-            } else {
-                totalCalories += it.toLong()
             }
+            maxTotalCalories
         }
-        return maxTotalCalories
-    }
 
-    fun part2(): Long {
-        data class Top3Elves(
-            var top1: Long = 0L,
-            var top2: Long = 0L,
-            var top3: Long = 0L,
-        ) {
-            fun update(value: Long) =
-                when {
-                    value <= top3 -> {}
-                    value > top1 -> {
-                        top3 = top2
-                        top2 = top1
-                        top1 = value
+    fun part2(): Long =
+        solve(2022) { lines ->
+            data class Top3Elves(
+                var top1: Long = 0L,
+                var top2: Long = 0L,
+                var top3: Long = 0L,
+            ) {
+                fun update(value: Long) =
+                    when {
+                        value <= top3 -> {}
+                        value > top1 -> {
+                            top3 = top2
+                            top2 = top1
+                            top1 = value
+                        }
+
+                        value in (top2 + 1) until top1 -> {
+                            top3 = top2
+                            top2 = value
+                        }
+
+                        value in (top3 + 1) until top2 -> {
+                            top3 = value
+                        }
+
+                        else -> {}
                     }
 
-                    value in (top2 + 1) until top1 -> {
-                        top3 = top2
-                        top2 = value
-                    }
+                fun totalCalories() = top1 + top2 + top3
+            }
 
-                    value in (top3 + 1) until top2 -> {
-                        top3 = value
-                    }
-
-                    else -> {}
+            val top3Elves = Top3Elves()
+            var totalCalories = 0L
+            lines.forEach {
+                if (it.isBlank()) {
+                    top3Elves.update(totalCalories)
+                    totalCalories = 0L
+                } else {
+                    totalCalories += it.toLong()
                 }
-
-            fun totalCalories() = top1 + top2 + top3
-        }
-
-        val top3Elves = Top3Elves()
-        var totalCalories = 0L
-        "2022/Day01.txt".readInputForEachLine {
-            if (it.isBlank()) {
-                top3Elves.update(totalCalories)
-                totalCalories = 0L
-            } else {
-                totalCalories += it.toLong()
             }
+
+            top3Elves.totalCalories()
         }
 
-        return top3Elves.totalCalories()
-    }
-
-    (part1() to 72017L).verify()
-    (part2() to 212520L).verify()
+//    (part1() to 72017L).verify()
+//    (part2() to 212520L).verify()
 }
---
+++ src/test/kotlin/aoc2022/Day01Test.kt
@@ -0,0 +1,16 @@
+package aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day01Test {
+    @Test
+    fun part1() {
+        assertEquals(72017L, Day01.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(212520L, Day01.part2())
+    }
+}
