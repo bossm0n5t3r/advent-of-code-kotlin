5bb3408d4d80c5e69e541fd32627e341ebfb6d69
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-02 03:55:00 +0900

    --- Day 2: Rock Paper Scissors ---

--- src/main/kotlin/aoc2022/Day02.kt
+++ src/main/kotlin/aoc2022/Day02.kt
@@ -1,7 +1,6 @@
 package aoc2022
 
-import utils.readInputForEachLine
-import utils.verify
+import utils.solve
 
 /**
  * --- Day 2: Rock Paper Scissors ---
@@ -11,67 +10,66 @@ import utils.verify
  * plus the score for the outcome of the round
  * (0 if you lost, 3 if the round was a draw, and 6 if you won).
  */
-fun main() {
-    fun part1(): Long {
-        val rockPaperScissors =
-            mapOf(
-                "A" to 1,
-                "B" to 2,
-                "C" to 3,
-                "X" to 1,
-                "Y" to 2,
-                "Z" to 3,
-            )
+object Day02 {
+    fun part1(): Long =
+        solve(2022) { lines ->
+            val rockPaperScissors =
+                mapOf(
+                    "A" to 1,
+                    "B" to 2,
+                    "C" to 3,
+                    "X" to 1,
+                    "Y" to 2,
+                    "Z" to 3,
+                )
 
-        val scores =
-            mapOf(
-                0 to 3,
-                1 to 6,
-                2 to 0,
-            )
+            val scores =
+                mapOf(
+                    0 to 3,
+                    1 to 6,
+                    2 to 0,
+                )
 
-        var totalScores = 0L
-        "2022/Day02.txt".readInputForEachLine {
-            val (opponentHand, myHand) =
-                it.split(" ").map { hand ->
-                    rockPaperScissors[hand] ?: error("NOT FOUND HANDS : $hand")
-                }
-            val score =
-                scores[(myHand - opponentHand + 3) % 3]
-                    ?: error("NOT FOUND SCORES - myHand: $myHand, opponentHand: $opponentHand")
-            totalScores += (myHand + score)
+            var totalScores = 0L
+            lines.forEach {
+                val (opponentHand, myHand) =
+                    it.split(" ").map { hand ->
+                        rockPaperScissors[hand] ?: error("NOT FOUND HANDS : $hand")
+                    }
+                val score =
+                    scores[(myHand - opponentHand + 3) % 3]
+                        ?: error("NOT FOUND SCORES - myHand: $myHand, opponentHand: $opponentHand")
+                totalScores += (myHand + score)
+            }
+            totalScores
         }
-        return totalScores
-    }
 
-    fun part2(): Long {
-        val rockPaperScissors =
-            mapOf(
-                "A" to 1,
-                "B" to 2,
-                "C" to 3,
-            )
-        val scores =
-            mapOf(
-                "X" to 0,
-                "Y" to 3,
-                "Z" to 6,
-            )
+    fun part2(): Long =
+        solve(2022) { lines ->
+            val rockPaperScissors =
+                mapOf(
+                    "A" to 1,
+                    "B" to 2,
+                    "C" to 3,
+                )
+            val scores =
+                mapOf(
+                    "X" to 0,
+                    "Y" to 3,
+                    "Z" to 6,
+                )
 
-        var totalScores = 0L
-        "2022/Day02.txt".readInputForEachLine {
-            val (opponentHandKey, scoreKey) = it.split(" ")
-            val opponentHand = rockPaperScissors[opponentHandKey] ?: error("NOT FOUND HANDS : $opponentHandKey")
-            val score = scores[scoreKey] ?: error("NOT FOUND SCORES - scoreKey: $scoreKey")
+            var totalScores = 0L
+            lines.forEach {
+                val (opponentHandKey, scoreKey) = it.split(" ")
+                val opponentHand = rockPaperScissors[opponentHandKey] ?: error("NOT FOUND HANDS : $opponentHandKey")
+                val score = scores[scoreKey] ?: error("NOT FOUND SCORES - scoreKey: $scoreKey")
 
-            val refinedScore = (score / 3 + 2) % 3
-            val refinedOpponentHand = (opponentHand + 1) % 3
-            val myHand = (refinedScore + refinedOpponentHand + 1) % 3 + 1
-            totalScores += (score + myHand)
+                val refinedScore = (score / 3 + 2) % 3
+                val refinedOpponentHand = (opponentHand + 1) % 3
+                val myHand = (refinedScore + refinedOpponentHand + 1) % 3 + 1
+                totalScores += (score + myHand)
+            }
+            totalScores
         }
-        return totalScores
-    }
-
-    (part1() to 12740L).verify()
-    (part2() to 11980L).verify()
 }
---
+++ src/test/kotlin/aoc2022/Day02Test.kt
@@ -0,0 +1,16 @@
+package aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day02Test {
+    @Test
+    fun part1() {
+        assertEquals(12740L, Day02.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(11980L, Day02.part2())
+    }
+}
