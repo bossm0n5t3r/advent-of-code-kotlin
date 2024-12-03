4b1cef35f2829df1904b45df4f794aaca5d47d04
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-03 20:45:00 +0900

    --- Day 4: Camp Cleanup ---

--- src/main/kotlin/aoc2022/Day04.kt
+++ src/main/kotlin/aoc2022/Day04.kt
@@ -1,42 +1,40 @@
 package aoc2022
 
-import utils.readInputForEachLine
-import utils.verify
+import utils.solve
 
 /**
  * --- Day 4: Camp Cleanup ---
  */
-fun main() {
-    fun part1(): Int {
-        var result = 0
-        "2022/Day04.txt".readInputForEachLine { line ->
-            val (firstSections, secondSections) =
-                line.split(",").map { sectionPair ->
-                    val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
-                    (startSection..endSection).toSet()
+object Day04 {
+    fun part1(): Int =
+        solve(2022) { lines ->
+            var result = 0
+            lines.forEach { line ->
+                val (firstSections, secondSections) =
+                    line.split(",").map { sectionPair ->
+                        val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
+                        (startSection..endSection).toSet()
+                    }
+                if (firstSections.containsAll(secondSections) || secondSections.containsAll(firstSections)) {
+                    result++
                 }
-            if (firstSections.containsAll(secondSections) || secondSections.containsAll(firstSections)) {
-                result++
             }
+            result
         }
-        return result
-    }
 
-    fun part2(): Int {
-        var result = 0
-        "2022/Day04.txt".readInputForEachLine { line ->
-            val (firstSections, secondSections) =
-                line.split(",").map { sectionPair ->
-                    val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
-                    (startSection..endSection).toSet()
+    fun part2(): Int =
+        solve(2022) { lines ->
+            var result = 0
+            lines.forEach { line ->
+                val (firstSections, secondSections) =
+                    line.split(",").map { sectionPair ->
+                        val (startSection, endSection) = sectionPair.split("-").map { intStr -> intStr.toInt() }
+                        (startSection..endSection).toSet()
+                    }
+                if (firstSections.intersect(secondSections).isNotEmpty()) {
+                    result++
                 }
-            if (firstSections.intersect(secondSections).isNotEmpty()) {
-                result++
             }
+            result
         }
-        return result
-    }
-
-    (part1() to 576).verify()
-    (part2() to 905).verify()
 }
---
+++ src/test/kotlin/aoc2022/Day04Test.kt
@@ -0,0 +1,16 @@
+package aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day04Test {
+    @Test
+    fun part1() {
+        assertEquals(576, Day04.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(905, Day04.part2())
+    }
+}
