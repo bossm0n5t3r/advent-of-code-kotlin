7b8a00f8bba7b1b8d2adb0cdccd62e477f8eb184
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-03 20:40:00 +0900

    --- Day 3: Rucksack Reorganization ---

--- src/main/kotlin/aoc2022/Day03.kt
+++ src/main/kotlin/aoc2022/Day03.kt
@@ -1,53 +1,51 @@
 package aoc2022
 
-import utils.readInputForEachLine
-import utils.verify
+import utils.solve
 
 /**
  * --- Day 3: Rucksack Reorganization ---
  */
-fun main() {
-    fun part1(): Long {
-        var totalPriorities = 0L
-        "2022/Day03.txt".readInputForEachLine {
-            val length = it.length
-            val firstCompartment = it.subSequence(0, length / 2).toSet()
-            val secondCompartment = it.subSequence(length / 2, length).toSet()
+object Day03 {
+    fun part1(): Long =
+        solve(2022) { lines ->
+            var totalPriorities = 0L
+            lines.forEach {
+                val length = it.length
+                val firstCompartment = it.subSequence(0, length / 2).toSet()
+                val secondCompartment = it.subSequence(length / 2, length).toSet()
 
-            val intersection = firstCompartment.intersect(secondCompartment).first()
-            totalPriorities +=
-                if (intersection.code >= 'a'.code) {
-                    intersection.code - 'a'.code + 1
-                } else {
-                    intersection.code - 'A'.code + 27
-                }
-        }
-        return totalPriorities
-    }
-
-    fun part2(): Long {
-        var totalPriorities = 0L
-        var cnt = 0
-        var intersection = ('a'..'z').toSet() + ('A'..'Z').toSet()
-        "2022/Day03.txt".readInputForEachLine {
-            val rucksack = it.toCharArray().toSet()
-            intersection = intersection.intersect(rucksack)
-            cnt++
-            if (cnt == 3) {
-                val badge = intersection.first()
+                val intersection = firstCompartment.intersect(secondCompartment).first()
                 totalPriorities +=
-                    if (badge.code >= 'a'.code) {
-                        badge.code - 'a'.code + 1
+                    if (intersection.code >= 'a'.code) {
+                        intersection.code - 'a'.code + 1
                     } else {
-                        badge.code - 'A'.code + 27
+                        intersection.code - 'A'.code + 27
                     }
-                intersection = ('a'..'z').toSet() + ('A'..'Z').toSet()
-                cnt = 0
             }
+            totalPriorities
         }
-        return totalPriorities
-    }
 
-    (part1() to 7826L).verify()
-    (part2() to 2577L).verify()
+    fun part2(): Long =
+        solve(2022) { lines ->
+            var totalPriorities = 0L
+            var cnt = 0
+            var intersection = ('a'..'z').toSet() + ('A'..'Z').toSet()
+            lines.forEach {
+                val rucksack = it.toCharArray().toSet()
+                intersection = intersection.intersect(rucksack)
+                cnt++
+                if (cnt == 3) {
+                    val badge = intersection.first()
+                    totalPriorities +=
+                        if (badge.code >= 'a'.code) {
+                            badge.code - 'a'.code + 1
+                        } else {
+                            badge.code - 'A'.code + 27
+                        }
+                    intersection = ('a'..'z').toSet() + ('A'..'Z').toSet()
+                    cnt = 0
+                }
+            }
+            totalPriorities
+        }
 }
---
+++ src/test/kotlin/aoc2022/Day03Test.kt
@@ -0,0 +1,16 @@
+package aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03Test {
+    @Test
+    fun part1() {
+        assertEquals(7826L, Day03.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(2577L, Day03.part2())
+    }
+}
